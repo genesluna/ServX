21603cdc1ba2106626ea6e8cc2c06124f094ba67
Genes Luna <genesluna@gmail.com>
2023-04-30 15:34:38 -0300

    added authprovider, safearea, routes and statusbar

--- App.tsx
+++ App.tsx
@@ -1,5 +1,16 @@
-import LoginSreen from "./src/screens/login";
+import "react-native-gesture-handler";
+import { AuthProvider } from "./src/context/AuthContext";
+import { Routes } from "./src/routes";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from "react-native";
 
 export default function App() {
-  return <LoginSreen />;
+  return (
+    <AuthProvider>
+      <SafeAreaView className="flex-1">
+        <Routes />
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    </AuthProvider>
+  );
 }
