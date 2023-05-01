f2d683093f3e11fa55662f46a0408daa5c8f8bfa
Genes Luna <genesluna@gmail.com>
2023-05-01 00:21:56 -0300

    added container component

---
+++ src/components/common/Container.tsx
@@ -0,0 +1,12 @@
+import { View, Text, ViewProps } from "react-native";
+import React from "react";
+
+const Container = ({ children, ...props }: ViewProps) => {
+  return (
+    <View className="items-center justify-center flex-1 px-10 bg-base-100" {...props}>
+      {children}
+    </View>
+  );
+};
+
+export default Container;
