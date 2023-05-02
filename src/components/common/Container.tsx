1e099e542b97b31f69456e099c389baa73161a3c
Genes Luna <genesluna@gmail.com>
2023-05-02 00:31:05 -0300

    added doc

--- src/components/common/Container.tsx
+++ src/components/common/Container.tsx
@@ -1,7 +1,15 @@
 import { View, ViewProps } from "react-native";
 import React from "react";
 
-const Container = ({ children, ...props }: ViewProps) => {
+/**
+ * A container component that centers its children and applies some base styling.
+ *
+ * @param {object} props - The props object.
+ * @param {ReactNode} props.children - The child elements to be rendered inside the container.
+ * @param {ViewProps} props.props - The additional props to be applied to the container view.
+ * @returns {JSX.Element} - A container component with centered children and base styling.
+ */
+const Container = ({ children, ...props }: ViewProps): JSX.Element => {
   return (
     <View className="items-center justify-center flex-1 px-10 bg-base-100" {...props}>
       {children}
