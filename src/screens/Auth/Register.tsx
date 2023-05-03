c52f829ca81ae4df8ffa9599133e94e635cba5b3
Genes Luna <genesluna@gmail.com>
2023-05-03 15:06:26 -0300

    added register screen

---
+++ src/screens/Auth/Register.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Keyboard, KeyboardAvoidingView, Platform, ToastAndroid, TouchableWithoutFeedback, View } from "react-native";
+
+import { useAuth } from "../../context/AuthContext";
+import RegisterForm, { RegisterFormValues } from "../../components/form/Auth/RegisterForm";
+import Header from "../../components/form/Auth/Header";
+import Container from "../../components/common/Container";
+
+const Register = () => {
+  const { register } = useAuth();
+
+  async function handleRegister({ name, email, password }: RegisterFormValues) {
+    try {
+      let result = await register(email, password);
+      await result.user.updateProfile({ displayName: name });
+    } catch (error: any) {
+      if (error.code === "auth/email-already-in-use") {
+        ToastAndroid.showWithGravity("Este email já está cadastrado", ToastAndroid.LONG, ToastAndroid.TOP);
+      }
+
+      if (error.code === "auth/invalid-email") {
+        ToastAndroid.showWithGravity("Email inválido", ToastAndroid.LONG, ToastAndroid.TOP);
+      }
+
+      console.log(error);
+    }
+  }
+
+  return (
+    <KeyboardAvoidingView className="flex-1" behavior={Platform.OS === "ios" ? "padding" : "height"}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Container>
+          <Header />
+          <RegisterForm onSubmit={handleRegister} />
+        </Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+  );
+};
+
+export default Register;
