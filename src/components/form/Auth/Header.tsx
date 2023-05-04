bd9d1a6ed3eefc47c65acfe66289cfa16f3ec1c6
Genes Luna <genesluna@gmail.com>
2023-05-04 00:00:25 -0300

    a few tweaks to doc

--- src/components/form/Auth/Header.tsx
+++ src/components/form/Auth/Header.tsx
@@ -6,10 +6,9 @@ import Logo from "../../../../assets/servx_logo_md.png";
 type HeaderProps = ViewProps & {};
 
 /**
- * A component that displays the ServX logo as the header.
+ * A component that displays the App logo as the header of the auth forms.
  *
- * @param {HeaderProps} props - The props object that extends ViewProps interface.
- * @returns {JSX.Element} - A JSX.Element representing the ServX header.
+ * @returns - A JSX.Element representing the auth form header.
  */
 const Header = ({ ...props }: HeaderProps): JSX.Element => {
   return (
