23aef71607104dcbd5f53fa756c2a40429a8ba7c
Genes Luna <genesluna@gmail.com>
2023-05-07 22:36:42 -0300

    added tenant service

---
+++ src/services/firestore/tenantService.ts
@@ -0,0 +1,31 @@
+import firestore, { FirebaseFirestoreTypes } from "@react-native-firebase/firestore";
+import { Memberships, Tenant } from "../../models/Tenant";
+
+const tenantsCollection = firestore().collection("tenants") as FirebaseFirestoreTypes.CollectionReference<Tenant>;
+
+function membershipCollection(tenantId: string): FirebaseFirestoreTypes.CollectionReference<Memberships> {
+  return firestore().collection(
+    `tenants/${tenantId}/memberships`
+  ) as FirebaseFirestoreTypes.CollectionReference<Memberships>;
+}
+
+export function createTenant(tenant: Tenant) {
+  return tenantsCollection.add({
+    ...tenant,
+    createdAt: firestore.FieldValue.serverTimestamp(),
+  });
+}
+
+export function createMembership(membership: Memberships, tenantId: string, userId: string) {
+  return membershipCollection(tenantId)
+    .doc(userId)
+    .set({
+      ...membership,
+    });
+}
+
+export function updateTenant(tenant: Tenant) {
+  return tenantsCollection.add({
+    ...tenant,
+  });
+}
