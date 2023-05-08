69442a7d18a7d3b24cf67673b98ce0be5a09c5d5
Genes Luna <genesluna@gmail.com>
2023-05-07 22:37:30 -0300

    updated user model

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,13 @@
+import { FirebaseFirestoreTypes } from "@react-native-firebase/firestore";
+
 export interface User {
-  name: string | null;
-  email: string | null;
-  photoURL: string | null;
+  name?: string | null;
+  email?: string | null;
+  photoURL?: string | null;
   phoneNumber?: string | null;
+  activeTenant?: {
+    tenantId?: string | null;
+    tenantName?: string | null;
+  };
+  createdAt?: FirebaseFirestoreTypes.FieldValue | null;
 }
