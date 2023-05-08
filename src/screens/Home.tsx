5de6e97924a125deefa89300823d226c3a5bca06
Genes Luna <genesluna@gmail.com>
2023-05-07 22:43:09 -0300

    added welcome msg for testing

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,18 +1,26 @@
-import { View, Text } from "react-native";
+import { Text } from "react-native";
 import React from "react";
 import Button from "../components/common/Button";
 import { useAuth } from "../context/AuthContext";
 import Container from "../components/common/Container";
-import Input from "../components/common/Input";
 
 type Props = {};
 
 const Home = (props: Props) => {
-  const { logout } = useAuth();
+  const { logout, appUser } = useAuth();
+
+  async function handleLogout(): Promise<void> {
+    try {
+      return await logout();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <Container>
-      <Text className="text-4xl">Home</Text>
-      <Button icon="log-out" label="logout" onPress={logout} />
+      <Text className="text-2xl my-6">{`Seja bem-vindo, ${appUser?.name}`}</Text>
+      <Button icon="log-out" label="logout" onPress={handleLogout} />
     </Container>
   );
 };
