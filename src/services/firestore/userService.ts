e624c8bf6496f9f833522251f4dc6a440ddb3f0f
Genes Luna <genesluna@gmail.com>
2023-05-07 22:39:32 -0300

    added update user function

--- src/services/firestore/userService.ts
+++ src/services/firestore/userService.ts
@@ -1,14 +1,16 @@
-import { FirebaseAuthTypes } from "@react-native-firebase/auth";
 import firestore, { FirebaseFirestoreTypes } from "@react-native-firebase/firestore";
 import { User } from "../../models/User";
 
-const usersCollection = firestore().collection("Users") as FirebaseFirestoreTypes.CollectionReference<User>;
+export const usersCollection = firestore().collection("users") as FirebaseFirestoreTypes.CollectionReference<User>;
 
-export function createUser(user: FirebaseAuthTypes.User): Promise<void> {
-  return usersCollection.doc(user.uid).set({
-    name: user.displayName,
-    email: user.email,
-    photoURL: user.photoURL,
-    phoneNumber: user.phoneNumber,
+export function createUser(user: User, userId: string): Promise<void> {
+  return usersCollection.doc(userId).set({
+    ...user,
+    createdAt: firestore.FieldValue.serverTimestamp(),
+  });
+}
+export function updateUser(user: User) {
+  return usersCollection.add({
+    ...user,
   });
 }
