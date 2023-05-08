a8756b8d5dc71fab60cb2892cfea8e27eb5f1cd2
Genes Luna <genesluna@gmail.com>
2023-05-07 22:32:45 -0300

    added tenant register and initial route

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,14 +1,16 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Login from "../screens/Auth/Login";
 import Register from "../screens/Auth/Register";
+import Tenant from "../screens/Auth/Tenant";
 
 const { Screen, Navigator } = createNativeStackNavigator();
 
 export function AuthRoutes() {
   return (
-    <Navigator>
+    <Navigator initialRouteName="login">
       <Screen name="login" component={Login} options={{ headerShown: false }} />
       <Screen name="register" component={Register} options={{ title: "Registro", headerTitleAlign: "center" }} />
+      <Screen name="tenant" component={Tenant} options={{ title: "Cadastro da Empresa", headerTitleAlign: "center" }} />
     </Navigator>
   );
 }
